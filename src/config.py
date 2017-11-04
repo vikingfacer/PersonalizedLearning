12e467ffd9490d29dd80cc9c11b332cd1a66a6bc
Daniel Santos <dsantosp12@gmail.com>
2017-11-04 17:49:45 -0400

    initial commit with back-end structure

---
+++ src/config.py
@@ -0,0 +1,15 @@
+import os
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+HOST = "0.0.0.0"
+PORT = 5000
+DEBUG = True
+
+SECRET_KEY = "11dab799-0be9-asedf4#$*&*(@LASD4fe-9ded-651273eab8d3"
+
+DB_NAME = "personalized_learning"
+USERNAME = "root"
+PASSWORD = "root"
+LOCATION = "localhost"
+PORT = "3306"
