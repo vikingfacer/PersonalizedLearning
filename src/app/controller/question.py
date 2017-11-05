fd9439638eb07d92e4181bd39dc3d5bcef7cba0b
Daniel Santos <dsantosp12@gmail.com>
2017-11-05 01:23:52 -0400

    added endpoints for mbti and lp questions

--- src/app/controller/question.py
+++ src/app/controller/question.py
@@ -6,17 +6,33 @@ from flask import Blueprint, request, Response
 from app.core.mbti import MBTI
 from app.model.user import User
 from app.model.questions import Question
+from app.model.response import Response as Answer
 
 
 question = Blueprint('question', __name__)
 
 
-@question.route('/mbti/questions')
-def fetch_mbti_questionnaire():
+@question.route('/questions')
+def fetch_questionnaire():
     return json.dumps(
         [question.to_dict() for question in Question.select()]
     )
 
+
+@question.route('/questions/mbti')
+def fetch_mbti_questionnaire():
+    return json.dumps(
+        [question.to_dict() for question in Question.select().where(Question.test_type == "MBTI")]
+    )
+
+
+@question.route('/questions/lp')
+def fetch_lp_questionnaire():
+    lp_questions = [question.to_dict() for question in Question.select().where(Question.test_type == "LP")]
+
+    for question in lp_questions:
+        question["answers"] = [answer.to_dict() for answer in Answer.select().where()]
+        
 
 @question.route('/mbti/questionnaire/<userid>', methods=['POST'])
 def questionnaire_results(userid):
@@ -29,7 +45,7 @@ def questionnaire_results(userid):
 
     user.color_id = mbti_results.color
     user.save()
-    
+
     return json.dumps(
         {
             "mbti_value": mbti_results.mbti_value,
