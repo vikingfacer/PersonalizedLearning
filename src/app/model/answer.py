2184424cebf1021b36dde5567f101d9ea74b2012
Daniel Santos <dsantosp12@gmail.com>
2017-11-05 01:23:52 -0500

    renamed response to answer

---
+++ src/app/model/answer.py
@@ -0,0 +1,25 @@
+import json
+from peewee import *
+
+from app import db
+from app.model.questions import Question
+
+
+class Answer(Model):
+    content = CharField()
+    question_id = ForeignKeyField(Question, to_field='id', related_name='response')
+
+    class Meta:
+        database = db
+
+    def __str__(self):
+        return json.dumps(self.__dict__["_data"])
+
+    def __repr__(self):
+        return self.__str__()
+
+    def to_dict(self):
+        JsonData = dict()
+        JsonData.update(self.__dict__["_data"])
+        JsonData.update(self.__dict__["_obj_cache"])
+        return JsonData
